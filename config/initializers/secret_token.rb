0d59c0bfa7a4972f84ce22e07e666176152bfcbf
Janko Jerinic <janko.jerinic@gmail.com>
2013-04-27 20:01:02 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crap101::Application.config.secret_token = '9b32d686fa558f20679a647a5cc51f3fcaebbdbb41e0cd54a9eadae41cfa3a4e97640912bf86d03bd00db561fce7c2639a678652e71b7ce7164a0eb04b3876e8'
